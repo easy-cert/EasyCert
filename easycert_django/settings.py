e543bba2a5e28280a27ed468d6cd6eecd0429093
Clark Kent Gomez <contactclark20@gmail.com>
2026-04-07 12:05:51 +0800

    feat: initialize Django project settings with custom apps, database configuration, and authentication backends

--- easycert_django/settings.py
+++ easycert_django/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'easycert_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'easycert',
+        'USER': 'easycert_user',
+        'PASSWORD': 'samplePass!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
